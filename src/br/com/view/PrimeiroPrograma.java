713faf36a1c2e0579ff664c9db94eb87242f7aef
obrenopereira <brenopereiragms@gmail.com>
2022-05-18 19:47:43 -0300

    first commit

---
+++ src/br/com/view/PrimeiroPrograma.java
@@ -0,0 +1,17 @@
+package br.com.view;
+
+import br.com.view.model.gato;
+
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+
+        gato Gato = new gato();
+
+        System.out.println(Gato);
+       /* int a = 5;
+        int b = 3;
+        System.out.println("Hello world!");*/
+    }
+
+}
